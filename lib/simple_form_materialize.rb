b43aee8b467000ad94453714f599e198cd27aa92
Christopher Pezza <pezza.chris@gmail.com>
2017-02-11 12:33:51 -0500

    Initial Commit

---
+++ lib/simple_form_materialize.rb
@@ -0,0 +1,5 @@
+require "simple_form_materialize/version"
+
+module SimpleFormMaterialize
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'simple_form_materialize'
+
+require 'minitest/autorun'
